c4a3640c667d6624c1b0ebf7e2ba7f94789786d2
jungwon <ixkfo86@gmail.com>
2025-05-09 23:43:38 +0900

    폴더삭제

--- work/nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  // GitHub Pages 배포를 위한 base URL 설정
-  app: {
-    baseURL: '/', // GitHub Pages 루트 도메인을 사용하므로 '/'로 설정
-  },
-  // ... existing code ...
-}) 
