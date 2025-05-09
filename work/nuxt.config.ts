d1821e2d7018735a96250a5115bd2e784023040d
jungwon <ixkfo86@gmail.com>
2025-05-09 23:26:16 +0900

    자동배포 테스트중

---
+++ work/nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // GitHub Pages 배포를 위한 base URL 설정
+  app: {
+    baseURL: '/portfolio/', // GitHub 저장소 이름과 동일하게 설정
+  },
+  // ... existing code ...
+}) 
