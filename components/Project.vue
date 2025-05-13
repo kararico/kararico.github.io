b126569daf8d37aa13264ff29dba96dadc6c2853
jungwon <ixkfo86@gmail.com>
2025-05-13 23:01:20 +0900

    작업중

--- components/Project.vue
+++ components/Project.vue
@@ -69,49 +69,49 @@ const projects = ref([
     skills: ['J-QUERY', 'SCSS', 'HTML5']
   },
   {
-    title: 'DANO SHOP',
-    description: '동일한 Class명을 사용하여 공통 디자인을 적용하고,비동기 데이터 통신을 활용하여 클론코딩한 다노샵 적응형 Mobile버전 웹사이트 입니다.',
-    link: 'https://ohl1234.github.io/danoshop2',
-    review: 'https://velog.io/@ohl1234/%EB%8B%A4%EB%85%B8%EC%83%B5-Clone-coding',
+    title: 'WCONCEPT US',
+    description: '더블유컨셉 US, 프레임워크 VUE.JS 변환 작업',
+    link: '',
+    review: '',
     logo: wconceptLogo,
     date: '2024.09 ~ 2025.03',
-    skills: ['SCSS', 'J-QUERY', 'Java-Script', 'JSON', 'Swiper']
+    skills: ['VUE.JS', 'Java-Script', 'SCSS' ]
   },
   {
-    title: 'Aesop',
-    description: 'Aesop 공식사이트와 GDWEB,Awwwards 등 레퍼런스 사이트를 참고하여 만든 Aesop 브랜딩 페이지 반응형 웹사이트 입니다.',
-    link: 'https://ohl1234.github.io/Aesop',
-    review: 'https://velog.io/@ohl1234/Aesop-%EB%B0%98%EC%9D%91%ED%98%95-%EB%A6%AC%EB%89%B4%EC%96%BC-%EC%82%AC%EC%9D%B4%ED%8A%B8-%EC%BD%94%EB%94%A9',
+    title: 'DISCOVERY',
+    description: '디스커버리 사이트 유지 운영',
+    link: 'https://www.discovery-expedition.com/',
+    review: '',
     logo: aesopLogo,
     date: '2024.09 ~ 2025.03',
-    skills: ['SCSS', 'Grid', 'J-QUERY', 'Java-Script', 'GSAP', 'Swiper']
+    skills: ['HTML5', 'SCSS', 'J-QUERY', 'GSAP',]
   },
   {
-    title: 'Dal.Komm',
-    description: '달콤커피 공식사이트와 GDWEB,Awwwards 등 레퍼런스 사이트를 참고하여 만든 적응형 PC 웹사이트 입니다.',
-    link: 'https://ohl1234.github.io/dalkomm',
-    review: 'https://velog.io/@ohl1234/DalKomm-coffee-%EC%A0%81%EC%9D%91%ED%98%95PC-%EB%A6%AC%EB%89%B4%EC%96%BC-%EC%BD%94%EB%94%A9',
+    title: 'MLB KOREA',
+    description: 'MLB KOREA 사이트 유지 운영',
+    link: 'https://www.mlb-korea.com/',
+    review: '',
     logo: dalkommLogo,
     date: '2024.09 ~ 2025.03',
-    skills: ['SCSS', 'J-QUERY', 'Java-Script', 'GSAP', 'Swiper']
+    skills: ['HTML5', 'SCSS', 'J-QUERY', 'GSAP',]
   },
   {
-    title: 'BAEMIN',
-    description: '배달의 민족 랜딩페이지 사이트와 GDWEB,Awwwards 등 레퍼런스 사이트를 참고하여 만든 반응형 웹사이트 입니다.',
-    link: 'https://ohl1234.github.io/baemin',
-    review: 'https://velog.io/@ohl1234/%EB%B0%B0%EB%8B%AC%EC%9D%98-%EB%AF%BC%EC%A1%B1-%EB%B0%98%EC%9D%91%ED%98%95-%EB%A6%AC%EB%89%B4%EC%96%BC-%ED%8E%98%EC%9D%B4%EC%A7%80-%EC%BD%94%EB%94%A9',
+    title: 'CASAMIA',
+    description: '까사미아 브랜드 사이트 구축',
+    link: 'https://casamia.co.kr/home',
+    review: '',
     logo: baeminLogo,
     date: '2024.09 ~ 2025.03',
     skills: ['SCSS', 'J-QUERY', 'Java-Script', 'GSAP']
   },
   {
-    title: 'Naver Webtoon',
-    description: '웹 접근성을 준수하고 비동기 데이터 통신을 활용하여 클론코딩한 네이버 웹툰 적응형 Mobile버전 웹사이트 입니다.',
-    link: 'https://ohl1234.github.io/naverwebtoon',
-    review: 'https://velog.io/@ohl1234/%EB%AA%A8%EB%B0%94%EC%9D%BC-%EC%A0%81%EC%9D%91%ED%98%95-%EB%84%A4%EC%9D%B4%EB%B2%84%EC%9B%B9%ED%88%B0-%ED%81%B4%EB%A1%A0%EC%BD%94%EB%94%A9',
+    title: 'Guud',
+    description: '라이프 스타일 이커머스 사이트 구축',
+    link: 'https://www.guud.co.kr/',
+    review: '',
     logo: webtoonLogo,
     date: '2024.09 ~ 2025.03',
-    skills: ['SCSS', 'J-QUERY', 'Java-Script', 'Swiper']
+    skills: ['SCSS', 'VUE.JS', 'Java-Script' ]
   }
 ])
 
