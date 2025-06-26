5f3c963c80d7398d59251541594c7cc2cde9db74
jungwon <ixkfo86@gmail.com>
2025-06-26 16:17:04 +0900

    nuxt 설정중

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,54 @@
 export default defineNuxtConfig({
   // GitHub Pages 배포를 위한 설정
   app: {
-    baseURL: '/kararico.github.io/',
-    buildAssetsDir: '_nuxt/',
-    cdnURL: ''
+    baseURL: '/',
+    head: {
+      title: '정원 포트폴리오 | JUNGWON Portfolio',
+      meta: [
+        { name: 'description', content: '2025년 웹퍼블리셔 정원의 포트폴리오 사이트입니다.' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { property: 'og:type', content: 'Website' },
+        { property: 'og:title', content: '정원 포트폴리오 | JUNGWON Portfolio' },
+        { property: 'og:url', content: 'https://kararico.github.io' },
+        { property: 'og:description', content: '2025년 웹퍼블리셔 정원의 포트폴리오 사이트입니다.' },
+        { property: 'og:image', content: 'https://kararico.github.io/images/og_img.png' },
+        { property: 'og:site_name', content: '정원 포트폴리오 | JUNGWON Portfolio' },
+      ],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard.css'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Zephyr:wght@400;500;600;700&display=swap'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Fahkwang:wght@400;500;600;700&display=swap'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff.css'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap'
+        }
+      ]
+    }
   },
   css: [
     '@/assets/scss/imports.scss',
